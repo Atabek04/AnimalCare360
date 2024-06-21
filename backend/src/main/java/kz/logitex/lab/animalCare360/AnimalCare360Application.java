24e7a351c3aac6b269fd4e4ced2a839f339ad3a5
Atabek04 <otabek.shadimatov@gmail.com>
2024-06-22 00:05:11 +0500

    first commit

---
+++ backend/src/main/java/kz/logitex/lab/animalCare360/AnimalCare360Application.java
@@ -0,0 +1,13 @@
+package kz.logitex.lab.animalCare360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCare360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalCare360Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kz/logitex/lab/animalCare360/AnimalCare360ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.logitex.lab.animalCare360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalCare360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
